5416e202e0582630a49c1d3f708a6edc9770144c
isaac-svg <isaacsakyi009@gmail.com>
2023-06-01 22:06:03 +0000

    end of day one

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import ThemeProvider from './context/ThemeContext'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/Home'
+import Signin from './routes/Signin'
+import Signup from './routes/Signup'
+import Account from './routes/Account'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [coins, setCoins] = useState([])
+  const url =`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en
+  `
 
+  useEffect(()=>{
+
+    axios.get(url).then((response)=>{
+      setCoins(response.data)
+      console.log(response.data )
+    })
+  },[url])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider>
+      <Navbar/> 
+      <Routes>
+        <Route path='/' element={<Home coins={coins}/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/account' element={<Account/>}/>
+      </Routes>
+    </ThemeProvider>
   )
 }
 
---
+++ src/routes/CoinPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinPage = ({coins}) => {
+  return (
+    <div>CoinPage</div>
+  )
+}
+
+export default CoinPage
